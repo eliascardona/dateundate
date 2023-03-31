b303d47c2baacf98612d462bff61b4adc5576f83
Elias Cardona <eliascardonarod@gmail.com>
2023-03-31 10:07:31 -0600

    Commit 13. Terminando el diseño y funcionamiento de la pagina 'stats', donde se muestran los ganadores de la app

--- pages/stats.js
+++ pages/stats.js
@@ -7,8 +7,9 @@ import {
 import styles from "../styles/stats.module.css"
 
 function stats() {
-    const [masLikes, setMasLikes] = useState({})
-    //Fetch de todos los posts
+    const [masLikes, setMasLikes] = useState(null)
+    const [primerosMatches, setPrimerosMatches] = useState(null)
+    //Fetch de los ganadores
     useEffect(() => {
         const unsubscribe = onSnapshot(doc(firestore, 'ganadores', 'porMasLikes'), (info) => {
             setMasLikes(info.data())
@@ -16,24 +17,42 @@ function stats() {
         return unsubscribe
     }, [])
     
-    // useEffect(() => {
-    //     const unsubscribe = onSnapshot(doc(firestore, 'ganadores', 'porPrimerosMatches'), (info) => {
-    //         setMasLikes(info.data())
-    //     })
-    //     return unsubscribe
-    // }, [])
+    useEffect(() => {
+        const unsubscribe = onSnapshot(doc(firestore, 'ganadores', 'porPrimerosMatches'), (info) => {
+            const data = info.data()
+            data.matches.sort((a, b) => a.timestamp - b.timestamp)
+            setPrimerosMatches(data)
+        })
+        return unsubscribe
+    }, [])
 
     return (
         <div className={styles.globalContainer}>
             <div className={styles.mainLy}>
                 <div>
-                    <h3>
-                        Más guapx hasta ahora:
-                        <br/>
-                        { masLikes.username} con {masLikes.likes} likes
-                    </h3>
+                    <h1 className={styles.title}>GANADORES</h1>
+                    <h1 style={{color:'red'}}>más guapx hasta ahora</h1>
+                    <h2>
+                        {
+                            masLikes ? 
+                            `${masLikes.username} con ${masLikes.likes} likes` : 
+                            "cargando..."
+                        }
+                    </h2>
                 </div>
                 <div>
+                    <h1 style={{color:'red'}}>primeros matches</h1>
+                    { 
+                        primerosMatches ?
+                        primerosMatches.matches.map((item, i) => {
+                           return(
+                                <h2 key={i}>
+                                    #{i+1}: {item.personas[0]} con {item.personas[1]}
+                                </h2>
+                            )
+                        }) : 
+                        <h2>cargando...</h2>
+                    }
                 </div>
             </div>
         </div>
